e7591fe5eb3f6b4fa8e4c4d1e5732ac605d0b6f1
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2024-07-13 17:06:08 +0200

    Solved

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ n = int(input())
 sequence = []
 i = 1
 while True:
+    # extend the sequence list by the element "i" repeated i times
     sequence += [i] * i
+    # if the length of the sequence is greater than or equal to n, print the first n elements of the sequence
     if len(sequence) >= n:
         print(*sequence[0:n])
         break
